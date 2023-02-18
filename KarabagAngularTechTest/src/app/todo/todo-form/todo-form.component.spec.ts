5be5ff0d9289e2172e499d55cc27b38235928f9c
Jeson Godilo <jbgodilo@gmail.com>
2023-02-18 12:09:15 +0800

    Fix unit test failure

--- KarabagAngularTechTest/src/app/todo/todo-form/todo-form.component.spec.ts
+++ KarabagAngularTechTest/src/app/todo/todo-form/todo-form.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TodoService } from 'src/app/services/todo.service';
 
 import { TodoFormComponent } from './todo-form.component';
 
@@ -8,7 +13,9 @@ describe('TodoFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoFormComponent ]
+      declarations: [ TodoFormComponent ],
+      imports: [HttpClientModule, RouterTestingModule, FormsModule, ReactiveFormsModule],
+      providers: [TodoService]
     })
     .compileComponents();
 
--- KarabagAngularTechTest/src/app/todo/todo-list/todo-list.component.spec.ts
+++ KarabagAngularTechTest/src/app/todo/todo-list/todo-list.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { TodoService } from 'src/app/services/todo.service';
+import { SearchFilterPipe } from 'src/app/shared/pipes/search-filter.pipe';
 
 import { TodoListComponent } from './todo-list.component';
 
@@ -8,7 +12,9 @@ describe('TodoListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
+      declarations: [ TodoListComponent, SearchFilterPipe ],
+      imports: [HttpClientModule, FormsModule],
+      providers: [TodoService]
     })
     .compileComponents();
 
